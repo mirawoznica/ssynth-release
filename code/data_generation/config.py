c1051c9876e418c1334500c2d9b0876e48b6fdb9
mirawoznica <m.woznica@student.fontys.nl>
2025-11-03 21:32:37 +0100

    m

--- code/data_generation/config.py
+++ code/data_generation/config.py
@@ -1,5 +1,9 @@
-sDir = '.cache/fict_repo/data/supporting_data/materials/'  # directory where the skin layer models will be stored
-sDir_lesion_ver0 = '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver0/'
-sDir_lesion_ver1 = '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver1/'
-sDir_hdri = '.cache/fict_repo/data/supporting_data/hdri/'  # directory where lighting HDRI files are stored
-param_dir = '.cache/fict_repo/data/supporting_data/'  # directory where the .csv files are stored
+import os
+from pathlib import Path
+home = Path.home()
+
+sDir = os.path.join(home, '.cache/fict_repo/data/supporting_data/materials/')  # directory where the skin layer models will be stored
+sDir_lesion_ver0 = os.path.join(home, '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver0/')
+sDir_lesion_ver1 = os.path.join(home, '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver1/')
+sDir_hdri = os.path.join(home, '.cache/fict_repo/data/supporting_data/hdri/') # directory where lighting HDRI files are stored
+param_dir = os.path.join(home, '.cache/fict_repo/data/supporting_data/')  # directory where the .csv files are stored
