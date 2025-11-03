599470a1c11cfcb5c5694236a52b5f0ea4aacdfb
mirawoznica <m.woznica@student.fontys.nl>
2025-11-03 20:49:55 +0100

    Fixed congif paths

--- code/data_generation/config.py
+++ code/data_generation/config.py
@@ -1,5 +1,5 @@
-sDir = '../../data/supporting_data/materials/'  # directory where the skin layer models will be stored
-sDir_lesion_ver0 = '../../data/supporting_data/materials/lesions_release/ver0/'
-sDir_lesion_ver1 = '../../data/supporting_data/materials/lesions_release/ver1/'
-sDir_hdri = '../../data/supporting_data/hdri/'  # directory where lighting HDRI files are stored
-param_dir = '../../data/supporting_data/'  # directory where the .csv files are stored
+sDir = '.cache/fict_repo/data/supporting_data/materials/'  # directory where the skin layer models will be stored
+sDir_lesion_ver0 = '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver0/'
+sDir_lesion_ver1 = '.cache/fict_repo/data/supporting_data/materials/lesions_release/ver1/'
+sDir_hdri = '.cache/fict_repo/data/supporting_data/hdri/'  # directory where lighting HDRI files are stored
+param_dir = '.cache/fict_repo/data/supporting_data/'  # directory where the .csv files are stored
